addbfae3afaf4a03c8057b2a5c61cf5187bd903c
Bence Kozma <kozmixb@gmail.com>
2025-10-04 21:51:00 +0100

    add disk readout

--- go.mod
+++ go.mod
@@ -5,11 +5,15 @@ go 1.24.0
 require github.com/labstack/echo/v4 v4.13.4
 
 require (
+	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
+	github.com/shirou/gopsutil/v3 v3.24.5 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
+	github.com/yusufpapurcu/wmi v1.2.4 // indirect
 	golang.org/x/crypto v0.42.0 // indirect
 	golang.org/x/net v0.44.0 // indirect
 	golang.org/x/sys v0.36.0 // indirect
--- src/stats.go
+++ src/stats.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"runtime"
 
 	"github.com/labstack/echo/v4"
+	"github.com/shirou/gopsutil/v3/disk"
 )
 
+const GIGABYTE float64 = 1024 * 1024 * 1024
+
 func homeController(c echo.Context) error {
+	mountPath := "C:\\" // linux = "/", windows = "C:\\"
+	usage, err := disk.Usage(mountPath)
+	if err != nil {
+		log.Fatalf("Error getting disk usage for %s: %v", mountPath, err)
+	}
+
 	return c.Render(200, "index", map[string]interface{}{
 		"cpu_count":           runtime.NumCPU(),
 		"cpu_freq":            600.,
@@ -15,10 +26,10 @@ func homeController(c echo.Context) error {
 		"cpu_mem_total":       232,
 		"cpu_mem_used":        0.43,
 		"cpu_percent":         1.8,
-		"disk_usage_free":     24.6,
-		"disk_usage_percent":  17.7,
-		"disk_usage_total":    31.3,
-		"disk_usage_used":     52.9,
+		"disk_usage_free":     fmt.Sprintf("%.2f", (float64(usage.Free) / GIGABYTE)),
+		"disk_usage_percent":  fmt.Sprintf("%.2f", usage.UsedPercent),
+		"disk_usage_total":    fmt.Sprintf("%.2f", (float64(usage.Total) / GIGABYTE)),
+		"disk_usage_used":     fmt.Sprintf("%.2f", (float64(usage.Used) / GIGABYTE)),
 		"sensor_temperatures": 52.6,
 	})
 }
