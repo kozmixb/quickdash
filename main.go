7c61f1f8b79d9502ee7888d93042c53843ee4582
Bence Kozma <kozmixb@gmail.com>
2025-10-04 17:01:03 +0100

    add default

--- main.go
+++ main.go
@@ -25,8 +25,24 @@ func newTemplate() *Templates {
 func main() {
 	e := echo.New()
 	e.Use(middleware.Logger())
+	e.Static("/static", "public")
 
 	e.Renderer = newTemplate()
-	routes(e)
+	e.GET("/", func(c echo.Context) error {
+		return c.Render(200, "index", map[string]interface{}{
+			"cpu_count":           4,
+			"cpu_freq":            "[ 600.0, 600.0, 1200.0 ]",
+			"cpu_mem_avail":       463953920,
+			"cpu_mem_free":        115789824,
+			"cpu_mem_total":       971063296,
+			"cpu_mem_used":        436252672,
+			"cpu_percent":         1.8,
+			"disk_usage_free":     24678121472,
+			"disk_usage_percent":  17.7,
+			"disk_usage_total":    31307206656,
+			"disk_usage_used":     5292728320,
+			"sensor_temperatures": 52.616,
+		})
+	})
 	e.Logger.Fatal(e.Start(":3000"))
 }
